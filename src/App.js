820701c07da104ab60d19f5f3dc6fb43558bce29
arvin <arvin_1028@163.com>
2024-10-14 19:03:40 +0800

    将路由、redux、antd主题的配置从 index.js 转移到 App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router';
+import { Provider } from 'react-redux';
+import store from './store';
+import { ConfigProvider, theme } from 'antd';
 
 function App() {
   return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        components: {
+          Tree: {
+            titleHeight: 35.980
+          }
+        }
+      }}>
+      <Provider store={store}>
 
-    <div className="App">
+        <div className="App">
 
-      <RouterProvider router={router}></RouterProvider>
-    </div>
+          <RouterProvider router={router}></RouterProvider>
+        </div>
+      </Provider>
+    </ConfigProvider>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,23 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from '@/App';
-import { Provider } from 'react-redux';
-import store from './store';
-import { ConfigProvider, theme } from 'antd';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ConfigProvider
-    theme={{
-      algorithm: theme.darkAlgorithm,
-      components: {
-        Tree: {
-          titleHeight: 35.980
-        }
-      }
-    }}>
-    <Provider store={store}><App /></Provider>
-  </ConfigProvider>
-
-);
+root.render(<App />);
 
